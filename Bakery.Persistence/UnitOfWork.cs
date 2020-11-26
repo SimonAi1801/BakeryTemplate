d191fe320f1d7b4e17d29bbbeec791ac509474ed
Simon Aichmayr <simon.aichmayr@trusteddecisions.com>
2020-11-26 15:26:06 +0100

    Implemented db validation

--- Bakery.Persistence/UnitOfWork.cs
+++ Bakery.Persistence/UnitOfWork.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Bakery.Core.Contracts;
 using Bakery.Core.Entities;
+using System.Collections.Generic;
 
 namespace Bakery.Persistence
 {
-  public class UnitOfWork : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _dbContext;
         private bool _disposed;
@@ -55,7 +56,29 @@ namespace Bakery.Persistence
         /// <param name="entity"></param>
         private async Task ValidateEntity(object entity)
         {
-         
+            ValidationContext validationContext;
+            if (entity is Product product)
+            {
+                validationContext = new ValidationContext(product);
+                if (await _dbContext.Products.AnyAsync(p => p.Id != product.Id
+                    && p.Name.Equals(product.Name) && p.ProductNr.Equals(product.ProductNr)))
+                {
+                    throw new ValidationException(new ValidationResult(
+                        $"Product mit dem Namen {product.Name} existiert bereits.",
+                        new List<string> { "Product" }), null, new List<string> { "Product" });
+                }
+            }
+            else if (entity is Order order)
+            {
+                validationContext = new ValidationContext(order);
+                if (await _dbContext.Orders.AnyAsync(o => o.Id != order.Id
+                    && o.OrderNr.Equals(order.OrderNr)))
+                {
+                    throw new ValidationException(new ValidationResult(
+                        $"Bestellung mit der Nummer {order.OrderNr} existiert bereits.",
+                        new List<string> { "Order" }), null, new List<string> { "Order" });
+                }
+            }
         }
 
         public async Task DeleteDatabaseAsync() => await _dbContext.Database.EnsureDeletedAsync();
